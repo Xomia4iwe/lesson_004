3a0fd3b579a3c14e6445d75bf8f731d516a802fa
Bondarenko <Homja4iwe@gmail.com>
2021-10-17 13:25:49 +0300

    Выполнил задание №5

--- 05_snowfall.py
+++ 05_snowfall.py
@@ -2,46 +2,42 @@
 
 import simple_draw as sd
 
+import random as r
 
-# На основе кода из практической части реализовать снегопад:
-# - создать списки данных для отрисовки N снежинок
-# - нарисовать падение этих N снежинок
-# - создать список рандомных длинн лучей снежинок (от 10 до 100) и пусть все снежинки будут разные
-
-N = 20
-
-# Пригодятся функции
-# sd.get_point()
-# sd.snowflake()
-# sd.sleep()
-# sd.random_number()
-# sd.user_want_exit()
-
-# TODO здесь ваш код
-while True:
-    sd.clear_screen()
-    pass
-    pass
-    pass
-    sd.sleep(0.1)
-    if sd.user_want_exit():
-        break
-
-sd.pause()
-
-# подсказка! для ускорения отрисовки можно
-#  - убрать clear_screen()
-#  - в начале рисования всех снежинок вызвать sd.start_drawing()
-#  - на старом месте снежинки отрисовать её же, но цветом sd.background_color
-#  - сдвинуть снежинку
-#  - отрисовать её цветом sd.COLOR_WHITE на новом месте
-#  - после отрисовки всех снежинок, перед sleep(), вызвать sd.finish_drawing()
+sd.resolution = (1200, 600)
 
 
-# 4) Усложненное задание (делать по желанию)
-# - сделать рандомные отклонения вправо/влево при каждом шаге
-# - сделать сугоб внизу экрана - если снежинка долетает до низа, оставлять её там,
-#   и добавлять новую снежинку
-# Результат решения см https://youtu.be/XBx0JtxHiLg
+def snowfall(n):
+    snowflakes = []
+
+    for _ in range(n):
+        snowflakes.append([r.randint(100, 1000), r.randint(550, 1600), r.randint(20, 40), r.random(), r.random(),
+                           r.randint(1, 179)])
+
+    while True:
+        sd.clear_screen()
+        for s in snowflakes:
+            x, y, length, factor_a, factor_b, factor_c = s
+            point = sd.get_point(x, y)
+            sd.start_drawing()
+            sd.snowflake(center=point, length=length, color=sd.background_color, factor_a=factor_a,
+                         factor_b=factor_b, factor_c=factor_c)
+            if y > 50:
+                s[1] -= 10
+                x -= sd.random_number(-25, 25)
+                point_fall = sd.get_point(x, y)
+                sd.snowflake(point_fall, length=length, color=sd.COLOR_WHITE, factor_a=factor_a,
+                             factor_b=factor_b, factor_c=factor_c)
+            else:
+                last_point = sd.get_point(x, y - 1)
+                sd.snowflake(last_point, length, color=sd.COLOR_WHITE, factor_a=factor_a,
+                             factor_b=factor_b, factor_c=factor_c)
+                y += sd.random_number(600, 800)
+
+        sd.finish_drawing()
+        sd.sleep(0.175)
+        if sd.user_want_exit():
+            break
 
 
+snowfall(20)
